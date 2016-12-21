4f4b9e0f1319e6b0367e425cccd553e7d1d5a155
Evgeny <gazdovsky@gmail.com>
2016-12-22 01:14:32 +0300

    bugfix

--- examples/client/pairWithDevice.js
+++ examples/client/pairWithDevice.js
@@ -2,8 +2,14 @@ var Remote = require('../../lib/client/Remote');
 
 var remote = new Remote('192.168.88.214');
 
-remote.pair().then(function() {
-  console.log('success!');
-}, function(err) {
-  console.log('failure',err);
-}).catch(console.error);
+remote
+    .pair()
+    .then(
+        function () {
+            console.log('success!');
+        },
+        function (err) {
+            console.log('failure', err);
+        }
+    )
+    .catch(console.error);

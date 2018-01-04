aff57cfdf8634ac4d4f3b54a44b3d7dd3b33f18c
Roger Kok <kok.roger@gmail.com>
2018-01-04 14:13:45 +0100

    Fix a lot of bugs problems with variable inheritance.

--- examples/client/loginToDevice.js
+++ examples/client/loginToDevice.js
@@ -8,11 +8,15 @@ var remote = new Remote('Apple-TV.local.');
 // remote.ctrlInit().then(function(serverInfo) {
 //   console.log('Got ctrlInit',serverInfo);
 // });
+//52CA7D0769D1059A1DA67D7A470949B3
+//F2E909A89C228711247653D4E97031C600DBCBEB
 
 remote.isHostUp().then(function() {
-  return remote.login('0xA50880EE02A0B77A');
+  return remote.login('1434FFDA1DB2E17C');
 }).then(function(sessionId) {
   console.log('Successfully Logged In (sessionId=' + sessionId + ')');
+
+  remote.sendLeftButton();
   // remote.logout().then(function() {
   //   console.log('Successfully Logged Out');
   // });
